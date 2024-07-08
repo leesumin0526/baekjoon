72e981b79185a2da7d50827e3a844f12aff199e7
MiN <81948815+leesumin0526@users.noreply.github.com>
2024-07-08 14:06:29 +0700

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,37 @@
+//2839
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> a[101];
+bool check[101];
+int cnt = 0;
+
+void dfs(int x){
+	check[x] = true;
+	for(int i = 0; i < a[x].size(); i++){
+		int y = a[x][i];
+		if(!check[y]){
+			dfs(y);
+			cnt++;
+		}
+	}	
+}
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, m;
+	cin >> n >> m;
+
+	for(int i = 0; i < m; i++){
+		int u, v;
+		cin >> u >> v;
+		a[u].push_back(v);
+		a[v].push_back(u);
+	}
+	dfs(1);
+	cout << cnt << '\n';
+}
